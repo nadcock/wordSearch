f50a820b0f7ad1db5b9cd6f4d1e4236a12e83c71
nickadcock-wf <nick.adcock@webfilings.com>
2013-07-31 12:44:18 -0700

    Adding support for wikipedia topic searches. Also including BeautifulSoup4 library

---
+++ cgiServer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import CGIHTTPServer
+import BaseHTTPServer
+
+class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
+    cgi_directories = ["/cgi"]
+
+PORT = 9999
+
+httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+print "serving at port", PORT
+httpd.serve_forever()
